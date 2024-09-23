6c617bf8cd70bf1c9dce1283929499063db2069f
Pedro <pedro.r.franco90@gmail.com>
2024-09-23 17:21:00 -0300

    criando movies

--- tests/e2e/leads.spec.js
+++ tests/e2e/leads.spec.js
@@ -8,12 +8,17 @@ const { openAsBlob } = require('fs')
 let landingPage
 let toast
 
+let randomName
+let randomEmail
+
+
 test.beforeEach(async({page})=>{
   landingPage = new LandingPage(page)
   toast = new Toast(page)
 
 
 })
+
 
 test('deve cadastrar um lead na fila de espera', async ({ page }) => {
 
@@ -25,6 +30,28 @@ test('deve cadastrar um lead na fila de espera', async ({ page }) => {
   await landingPage.subitLeadForm(randomName,randomEmail)
 
   const message = 'Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!'
+  await toast.haveText(message)
+
+});
+
+test('não deve cadastrar quando o email já existe', async ({ page, request }) => {
+  const randomName = faker.person.fullName()
+  const randomEmail = faker.internet.email()
+
+const newLead = await request.post('http://localhost:3333/leads',{
+    data:{
+      name:randomName,
+      email:randomEmail
+    }
+  })
+
+  expect(newLead.ok()).toBeTruthy()
+
+  await landingPage.visit()
+  await landingPage.openLeadModal()
+  await landingPage.subitLeadForm(randomName,randomEmail)
+
+  const message = 'O endereço de e-mail fornecido já está registrado em nossa fila de espera.'
   await toast.haveText(message)
 
 });
---
+++ tests/e2e/movies.spec.js
@@ -0,0 +1,6 @@
+const { test } = require('@playwright/test')
+
+test('deve poder cadastrar um novo filme', ({ page }) => {
+
+
+})
