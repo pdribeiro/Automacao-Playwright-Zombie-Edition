74a347a004629596833ca06031b477454a29c9fd
Pedro <pedro.r.franco90@gmail.com>
2024-11-12 15:05:14 -0300

    refatondo o page objt

--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -1,35 +1,19 @@
-const { test } = require('@playwright/test')
+///exemplo utilizando //suport
 
+const { test } = require('../support')
 const data = require('../support/fixtures/movies.json')
-
 const {executeSQl} = require('../support/database')
-
-const { LoginPage } = require('../pages/LoginPage')
-const {MoviesPage} = require('../pages/MoviesPage')
-const { Toast } = require('../pages/Components')
-
-let loginPage
-let toast
-let moviesPage
-
-test.beforeEach(({ page }) => {
-    loginPage = new LoginPage(page)
-    moviesPage = new MoviesPage(page)
-    toast = new Toast(page)
-
-})
 
 test('deve poder cadastrar um novo filme',async ({ page }) => {
 
     const movie = data.create
-
     await executeSQl(`DELETE from movies WHERE title = '${movie.title}';`)
 
-    await loginPage.visit()
-    await loginPage.submit('admin@zombieplus.com', 'pwd123')
-    await moviesPage.isLoggedIn()
+    await page.login.visit()
+    await page.login.submit('admin@zombieplus.com', 'pwd123')
+    await page.movies.isLoggedIn()
 
-    await moviesPage.create(movie.title, movie.overview ,movie.company, movie.release_year)
+    await page.movies.create(movie.title, movie.overview ,movie.company, movie.release_year)
 
-    await toast.containText('Cadastro realizado com sucesso!')
+    await page.toast.containText('Cadastro realizado com sucesso!')
 })
